d167052dda9b59bc6934a410940ba4c6c61a941a
TiagoMagBorges <tiagomagborges@gmail.com>
2025-04-02 15:53:25 -0300

    Implemented domain layer

---
+++ src/main/java/br/com/dio/apirest/domain/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package br.com.dio.apirest.domain.repository;
+
+import br.com.dio.apirest.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
